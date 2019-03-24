cb43067d387232631d17eaa567505e1d980a34e5
Carla Balbontin <carlabalbontin@hotmail.com>
2019-03-23 21:31:34 -0300

    Adding GitHub URL in build process

---
+++ precache-manifest.9d529df8f7586587837fab2d9f17bc80.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c249308ef49bbd905ef2b3633f8ab11",
+    "url": "/PWA-ReactJS/static/media/loader.1c249308.gif"
+  },
+  {
+    "revision": "c21f3934a32c1241fb9eb92a41da6356",
+    "url": "/PWA-ReactJS/static/media/header.c21f3934.png"
+  },
+  {
+    "revision": "f7d569d4ea7a66662e09",
+    "url": "/PWA-ReactJS/static/js/runtime~main.eddf8bb9.js"
+  },
+  {
+    "revision": "c9e35682ca15ef712f2f",
+    "url": "/PWA-ReactJS/static/js/main.ce9380f7.chunk.js"
+  },
+  {
+    "revision": "c76e111d53806c8193e5",
+    "url": "/PWA-ReactJS/static/js/2.094d08f2.chunk.js"
+  },
+  {
+    "revision": "c9e35682ca15ef712f2f",
+    "url": "/PWA-ReactJS/static/css/main.8487c8de.chunk.css"
+  },
+  {
+    "revision": "075703f615d34f14edb112294bac34ab",
+    "url": "/PWA-ReactJS/index.html"
+  }
+];
