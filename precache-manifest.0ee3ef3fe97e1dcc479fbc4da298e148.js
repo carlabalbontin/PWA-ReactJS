231364edcfd428a2806eddc6fe3a2961dbf0828c
Carla Balbontin <carlabalbontin@hotmail.com>
2019-03-23 21:13:27 -0300

    Initial commit

---
+++ precache-manifest.0ee3ef3fe97e1dcc479fbc4da298e148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c249308ef49bbd905ef2b3633f8ab11",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/media/loader.1c249308.gif"
+  },
+  {
+    "revision": "c21f3934a32c1241fb9eb92a41da6356",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/media/header.c21f3934.png"
+  },
+  {
+    "revision": "b52268278bffcbf02211",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/js/runtime~main.eff7a220.js"
+  },
+  {
+    "revision": "e203903c83e9beea1a85",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/js/main.a1a5cdf8.chunk.js"
+  },
+  {
+    "revision": "c76e111d53806c8193e5",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/js/2.094d08f2.chunk.js"
+  },
+  {
+    "revision": "e203903c83e9beea1a85",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/static/css/main.8487c8de.chunk.css"
+  },
+  {
+    "revision": "114ccafdeeea269df3f48e69cb4f9ad7",
+    "url": "/Users/carlabalbontin/Documents/pwa/my-app/build/index.html"
+  }
+];
