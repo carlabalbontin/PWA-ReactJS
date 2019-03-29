21b581733f4c5e4fca48a0850bbefc5cb6692db6
Charlie <carlabalbontin@hotmail.com>
2019-03-28 21:17:27 -0300

    Updates

---
+++ precache-manifest.4c608ac1a96ac60de6090322d1a32743.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "1c249308ef49bbd905ef2b3633f8ab11",
+    "url": "/carlabalbontin/PWA-ReactJS/static/media/loader.1c249308.gif"
+  },
+  {
+    "revision": "c21f3934a32c1241fb9eb92a41da6356",
+    "url": "/carlabalbontin/PWA-ReactJS/static/media/header.c21f3934.png"
+  },
+  {
+    "revision": "64fc8239596f08ef9fe6",
+    "url": "/carlabalbontin/PWA-ReactJS/static/js/runtime~main.a00a8548.js"
+  },
+  {
+    "revision": "2be1b942e2b60aa38791",
+    "url": "/carlabalbontin/PWA-ReactJS/static/js/main.0b905c7e.chunk.js"
+  },
+  {
+    "revision": "c76e111d53806c8193e5",
+    "url": "/carlabalbontin/PWA-ReactJS/static/js/2.094d08f2.chunk.js"
+  },
+  {
+    "revision": "2be1b942e2b60aa38791",
+    "url": "/carlabalbontin/PWA-ReactJS/static/css/main.db544bd5.chunk.css"
+  },
+  {
+    "revision": "7a8a00897712e28b48e4961a6fe07ad6",
+    "url": "/carlabalbontin/PWA-ReactJS/index.html"
+  }
+];
